4cef7a9c9a1aec794fa332df558c15b22cb3d857
Mateusz Piwek <barnaba@200ms.net>
2025-06-12 16:53:29 +0200

    Re-vision

--- mpy_init/target.py
+++ mpy_init/target.py
@@ -14,32 +14,41 @@ class Sysinit(Target):
 # 1. Mount Essential Filesystems:
 # /proc, /sys, /dev, /run
 # Necessary for system information and device access.
+# Handled by: sysfs.py procfs.py dev_mount.py
 
 # 2. Perform FS checks and re-mount file systems to rw.
 # Checks and repairs (if necessary) file systems. Mount as read-write + initialize and configure swap space.
+# Handled by: fsck.py
 
 # 3. Load additional kernel modules (some modules, e.g. fs support is usually loaded by initramfs)
 # This includes network devices, power managment, GPU etc.
+# Handled by: modules.py
 
 # 4. Set hostname and console
 # Initializes console settings (fonts, keyboard layout).
+# NOT implemented
 
 # 5. Start Device Management
 # Starts udev (or equivalent) to manage dynamic device creation in /dev.
 # Handles hotplugging and initialization of devices.
 # Also it’s a time to start logging daemon.
+# Handled by: mdevd.service
 
 # 6. Setup networking
 # Starts DHCP clients, applies static Ips.
 # Perfect time to start firewall.
+# Handled by: net.py, nft.service
 
 # 7. Start System Services and Daemons
-#          sshd, cron (systemd timer), dbus, nginx, mysql
+#          sshd, cron (systemd timer)
+# Handled by:
+#     - dropbear.service
+#     - timers.py
 
 # 8. Setup logins and user sessions
 #     Starts virtual terminals (getty) for local logins.
 #     Starts display manager (e.g., GDM, LightDM)
-
+# NOT implemented
 
 class Basic(Target):
     pass
