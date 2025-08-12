9ca9dd1f22868734485f17084a66e16288019885
Mateusz Piwek <barnaba@200ms.net>
2025-08-12 19:15:21 +0200

    node_prototype WIP

--- mpy_init/core/logic/Node.py
+++ mpy_init/core/logic/Node.py
@@ -1,20 +1,22 @@
 
 
 class Node:
+
     _description: str
 
     # Start service after "__after" but does not imply a dependency.
-    __after: tuple  # Unit
+    __after = None  # Unit
     # Start service before "__before".
-    __before: tuple  # Unit
+    __before = None  # Unit
 
     # Weak dependency, if "__wants" service is missing, starts anyway.
-    __wants: tuple  # Unit
+    __wants = None # Unit
     # Strong dependency, "__requires" is requied to start service.
-    __requires: tuple  # Unit
+    __requires = None  # Unit
 
-    __provides: tuple  # Target
-    __defines: tuple
+    __provides = None  # Target
+    __defines = None
+
 
     def __init__(self, name: str, after: list[str], requires: list[str],
                  wants: list[str], provides: str, defines: str):
--- mpy_init/core/node_prototype.py
+++ mpy_init/core/node_prototype.py
@@ -8,7 +8,7 @@ class NodeSet:
         pass
 
     def addWeakDep(self):
-
+        pass
 
 class NodePrototype:
     # Unit description:
@@ -21,7 +21,58 @@ class NodePrototype:
     _provides: list = None
     _defines: str = None
 
+    def __init__(self, name):
+        self._deps = []
+
+    def prepare(self, nmark, ndeps):
+        mark = None
+        for v in self._deps:
+            if v in self._deps:
+                return True
+
+        self._deps.append(ndeps)
 
 
-    def __init__(self):
-        pass
+    def get_description(self):
+        return self._description
+    def set_description(self, value):
+        self._description = value
+
+    def get_after(self):
+        return self._after
+    def set_after(self, lst):
+        self._after = self.prepare(lst)
+
+    def get_before(self):
+        return self._before
+    def set_before(self, lst):
+        self._before = self.prepare(lst)
+
+    def get_wants(self):
+        return self._wants
+    def set_wants(self, lst):
+        self._wants = self.prepare(lst)
+
+    def get_requires(self):
+        return self._requires
+    def set_requires(self, lst):
+        self._requires = self.prepare(lst)
+
+    def get_provides(self):
+        return self._provides
+    def set_provides(self, lst):
+        self._provides = self.prepare(lst)
+
+    def get_defines(self):
+        return self._defines
+    def set_defines(self, name):
+        self._defines = name
+
+    description = property(get_description, set_description)
+    after = property(get_after, set_after)
+    before = property(get_before, set_before)
+    wants = property(get_wants, set_wants)
+    requires = property(get_requires, set_requires)
+    provides = property(get_provides, set_provides)
+    defines = property(get_defines, set_defines)
+
