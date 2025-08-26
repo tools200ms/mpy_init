b35b4f5c12630ab7cef0c6124ef0c742d444f945
Mateusz Piwek <barnaba@200ms.net>
2025-08-26 17:41:57 +0200

    Benchmark tune

--- benchmarks/py_threads.py
+++ benchmarks/py_threads.py
@@ -61,7 +61,7 @@ def cpu_stress_thread(name, result, lock=None):
 
 
 # 'const()' function to define constants in MicroPython.
-PRIME_NUMBER_TOTAL_CNT = const(6300)
+PRIME_NUMBER_TOTAL_CNT = const(1600)
 
 print(f"Python implementation: {sys.implementation.name}")
 
