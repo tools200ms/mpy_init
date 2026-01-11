b97be8d4265522f628bfcfdaf31d054a3e88af7b
Mateusz Piwek <barnaba@200ms.net>
2026-01-11 16:23:23 +0100

    Cython code that compiles

--- experiments/cython/hyinit-mini/__main__.py
+++ experiments/cython/hyinit-mini/__main__.py
@@ -1,22 +1,31 @@
 
-from cysignals.signals import sig_check
+#from cysignals.signals import sig_check
 from time import sleep
 import sys
 
+import logging
 
-def main():
+logging.basicConfig(level=logging.INFO)
+logging.info("Hello")
 
-    print("Hello")
-    try:
-        # Main program loop
-        while True:
-            sleep(5)
-            sig_check()
-    except Exception as e:
-        print(f"Error: {e}")
-        sys.exit(1)
+# def main():
+#
+#     print("Hello",  flush=True)
+#     try:
+#         # Main program loop
+#         while True:
+#             sleep(5)
+#             sig_check()
+#             sys.exit(5)
+#     except Exception as e:
+#         print(f"Error: {e}")
+#         sys.exit(1)
 
 
-print("Hello" + __name__)
-if __name__ == "__main__":
-    main()
+print("Hello" + __name__,  flush=True)
+sys.stdout.flush()
+
+sys.exit(56)
+
+# if __name__ == "__main__":
+#     main()
