7e46bae3633cef6e3dfd9070234273acdda80b1a
Mateusz Piwek <mp@200ms.net>
2026-06-12 19:54:21 +0200

    WIP

---
+++ setup/yinit-entry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+# Get path from environment variable or use default
+path = os.environ.get('YINIT_ROOT', '/usr/local/yinit')
+
+if not os.path.exists(path):
+    raise FileNotFoundError(f"YInit root path does not exist, YINIT_ROOT: {path}")
+if not os.path.isdir(path):
+    raise NotADirectoryError(f"Incorrect YInit root path, is should be a directory, YINIT_ROOT: {path}")
+
+sys.path.insert(0, path)
+
+from mpy_init.__main__ import main
+
+main()
