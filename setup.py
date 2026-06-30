b5f84177db3eedbae95aee6468073c2ea69ef5d7
Mateusz Piwek <barnaba@200ms.net>
2026-06-30 13:08:12 +0200

    Cythonization to .so

--- setup.py
+++ setup.py
@@ -2,22 +2,29 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 # Make Cython to generate a C file with a main() function:
-options = {"compiler_directives": {"language_level": "3"}}
+options = {
+    "compiler_directives": {"language_level": "3"}
+}
 
 extensions = [
     Extension(
     "yinit.main",
-    ["yinit/main.py"],
+    ["src/yinit/main.py"],
     ),
     Extension(
     "yinit.runenv",
-    ["yinit/runenv.py"],
+    ["src/yinit/runenv.py"],
     )
 ]
 
-# setup(
-#     name="yinit",
-#     ext_modules=cythonize(extensions, language_level = 3),
-#     package_dir={"": "src"},
-#     packages=["yinit"],
-# )
+setup(
+    name="yinit",
+    ext_modules=cythonize(extensions, **options),
+    package_dir={"": "src"},
+    packages=["yinit"],
+    entry_points={
+        "console_scripts": [
+            "yinit=main:main",
+        ]
+    },
+)
